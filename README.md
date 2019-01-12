6d4cbd6ad2d6cb0372338ff08f2ede89a168795a
Eko Rudiawan Jamzuri <eko.rudiawan@gmail.com>
2019-01-12 21:22:54 +0800

    Added mouse click event and documentation

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # CCD-Inverse-Kinematics-2D
-Cyclic Coordinate Descent Inverse Kinematics of n-Link Arm Robot in 2D Space with Python and Matplotlib
+
+Cyclic Coordinate Descent Inverse Kinematics of n-Link Arm Robot in 2D Space with Python and Matplotlib.
+
+Question : eko.rudiawan@gmail.com
+
+## Animation
+
+![CCD Inverse Kinematics](images/animation.gif)
+
+## Requirement
+
+1. Python 3
+2. Numpy
+3. Matplotlib
+
+## How to Change Total Link and Length of the Link
+
+```python
+# Example robot link parameter
+# Total link = 4
+# Length link_0 = 50
+# Length link_1 = 40
+# Length link_2 = 35
+# Length link_3 = 30
+link = [50, 40, 35, 30]
+```
--- sources/CCD-Inverse-Kinematics-2D.py
+++ sources/CCD-Inverse-Kinematics-2D.py
@@ -2,6 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import math
 
+# Robot Link Length Parameter
+link = [50, 40, 35, 30]
+# Robot Initial Joint Values (degree)
+angle = [0, 0, 0, 0]
+# Target End of Effector Position
+target = [0, 0, 0] 
+
+# Create figure to plot
+fig = plt.figure() 
+ax = fig.add_subplot(1,1,1)
+
 # Draw Axis
 def draw_axis(ax, scale=1.0, A=np.eye(4), style='-', draw_2d = False):
     xaxis = np.array([[0, 0, 0, 1], 
@@ -37,108 +48,116 @@ def translate(dx, dy, dz):
                   [0, 0, 0, 1]])
     return t
 
+# Forward Kinematics
+# Input initial angles and length of links
+# Output positions each points
 def FK(angle, link):
-    P0 = np.eye(4)
-    R0_1 = rotateZ(angle[0]/180*math.pi)
-    T0_1 = translate(link[0], 0, 0)
-    P1 = P0.dot(R0_1).dot(T0_1)
-    R1_2 = rotateZ(angle[1]/180*math.pi)
-    T1_2 = translate(link[1], 0, 0)
-    P2 = P1.dot(R1_2).dot(T1_2)
-    R2_3 = rotateZ(angle[2]/180*math.pi)
-    T2_3 = translate(link[2], 0, 0)
-    P3 = P2.dot(R2_3).dot(T2_3)
-    return P0, P1, P2, P3
+    n_links = len(link)
+    P = []
+    P.append(np.eye(4))
+    for i in range(0, n_links):
+        R = rotateZ(angle[i]/180*math.pi)
+        T = translate(link[i], 0, 0)
+        P.append(P[-1].dot(R).dot(T))
+    return P
 
 def IK(target, angle, link, max_iter = 10000, err_min = 0.1):
     solved = False
+    err_end_to_target = math.inf
+    
     for loop in range(max_iter):
-        P0, P1, P2, P3 = FK(angle, link)
-        list_joint_pos = [P2, P1, P0]
-        i = len(list_joint_pos) - 1
-        for joint_pos in list_joint_pos:
-            cur_to_end = P3[:3, 3] - joint_pos[:3, 3]
-            cur_to_end_mag = math.sqrt(cur_to_end[0] ** 2 + cur_to_end[1] ** 2)
-            cur_to_target = target - joint_pos[:3, 3]
-            cur_to_target_mag = math.sqrt(cur_to_target[0] ** 2 + cur_to_target[1] ** 2)
-            # print("cur_to_end :", cur_to_end)
-            # print("cur_to_end_mag :", cur_to_end_mag)
-            # print("cur_to_tgt :", cur_to_target)
-            # print("cur_to_tgt_mag :", cur_to_target_mag)
-
-            end_target_mag = cur_to_end_mag * cur_to_target_mag
-
-            if end_target_mag <= 0.0001:    # prevent division by small numbers
-                cos_rot_ang = 1
-                sin_rot_ang = 0
-            else:
-                cos_rot_ang = (cur_to_end[0] * cur_to_target[0] + cur_to_end[1] * cur_to_target[1]) / end_target_mag
-                sin_rot_ang = (cur_to_end[0] * cur_to_target[1] - cur_to_end[1] * cur_to_target[0]) / end_target_mag
-
-            # print("cos_rot_ang :", cos_rot_ang)
-            rot_ang = math.acos(max(-1, min(1,cos_rot_ang)))
-            # print("rot_ang :", rot_ang)
-            if sin_rot_ang < 0.0:
-                rot_ang = -rot_ang
-
-            # Disini untuk buat kondisi link
-            angle[i] = angle[i] + (rot_ang * 180 / math.pi)
-            if angle[i] >= 360:
-                angle[i] = angle[i] - 360
-            if angle[i] < 0:
-                angle[i] = 360 + angle[i]
-
-            _, _, _, P3 = FK(angle, link)
-
-            end_to_target = target - P3[:3, 3]
-            # print("end_to_tgt :", end_to_target)
+        for i in range(len(link)-1, -1, -1):
+            P = FK(angle, link)
+            end_to_target = target - P[-1][:3, 3]
             err_end_to_target = math.sqrt(end_to_target[0] ** 2 + end_to_target[1] ** 2)
-            # print("err :", err_end_to_target)
             if err_end_to_target < err_min:
-                solved = True      
-            i -= 1
+                solved = True
+            else:
+                # Calculate distance between i-joint position to end effector position
+                # P[i] is position of current joint
+                # P[-1] is position of end effector
+                cur_to_end = P[-1][:3, 3] - P[i][:3, 3]
+                cur_to_end_mag = math.sqrt(cur_to_end[0] ** 2 + cur_to_end[1] ** 2)
+                cur_to_target = target - P[i][:3, 3]
+                cur_to_target_mag = math.sqrt(cur_to_target[0] ** 2 + cur_to_target[1] ** 2)
+
+                end_target_mag = cur_to_end_mag * cur_to_target_mag
+
+                if end_target_mag <= 0.0001:    
+                    cos_rot_ang = 1
+                    sin_rot_ang = 0
+                else:
+                    cos_rot_ang = (cur_to_end[0] * cur_to_target[0] + cur_to_end[1] * cur_to_target[1]) / end_target_mag
+                    sin_rot_ang = (cur_to_end[0] * cur_to_target[1] - cur_to_end[1] * cur_to_target[0]) / end_target_mag
+
+                rot_ang = math.acos(max(-1, min(1,cos_rot_ang)))
+
+                if sin_rot_ang < 0.0:
+                    rot_ang = -rot_ang
+
+                # Update current joint angle values
+                angle[i] = angle[i] + (rot_ang * 180 / math.pi)
+
+                if angle[i] >= 360:
+                    angle[i] = angle[i] - 360
+                if angle[i] < 0:
+                    angle[i] = 360 + angle[i]
+                  
         if solved:
             break
+            
     return angle, err_end_to_target, solved, loop
 
-def main():
-    l1, l2, l3 = 50, 40, 35
-    t0, t1 = 20, 30
-    t2 = -(t0+t1)
-
-    # Robot Parameter
-    angle = [t0, t1, t2]
-    link = [l1, l2, l3]
-    target = [80, 80, 0] 
-
-    # Create figure to plot
-    fig = plt.figure() 
-    ax = fig.add_subplot(1,1,1)
-    ax.set_xlim(-50, 130)
-    ax.set_ylim(-50, 130)
-
-    # Forward Kinematics
-    P0, P1, P2, P3 = FK(angle, link)
-    ax.plot([P0[0,3], P1[0,3]], [P0[1,3], P1[1,3]])
-    ax.plot([P1[0,3], P2[0,3]], [P1[1,3], P2[1,3]])
-    ax.plot([P2[0,3], P3[0,3]], [P2[1,3], P3[1,3]])
+# Have not implemented
+def onclick(event):
+    global target, link, angle, ax
+    target[0] = event.xdata
+    target[1] = event.ydata
     
-    angle, err, solved, iteration = IK(target, angle, link, max_iter=100)
-    P0, P1, P2, P3 = FK(angle, link)
-    ax.plot([P0[0,3], P1[0,3]], [P0[1,3], P1[1,3]])
-    ax.plot([P1[0,3], P2[0,3]], [P1[1,3], P2[1,3]])
-    ax.plot([P2[0,3], P3[0,3]], [P2[1,3], P3[1,3]])
+    print("Target Position : ", target)
+    plt.cla()
+    ax.set_xlim(-50, 150)
+    ax.set_ylim(-50, 150)
+
+    # Inverse Kinematics
+    angle, err, solved, iteration = IK(target, angle, link, max_iter=1000)
+    
+    P = FK(angle, link)
+    for i in range(len(link)):
+        start_point = P[i]
+        end_point = P[i+1]
+        ax.plot([start_point[0,3], end_point[0,3]], [start_point[1,3], end_point[1,3]], linewidth=5)
+        # draw_axis(ax, scale=5, A=P[i+1], draw_2d=True)
+
     if solved:
+        print("\nIK solved\n")
         print("Iteration :", iteration)
         print("Angle :", angle)
         print("Target :", target)
-        print("EEF :", P3[:3, 3])
+        print("End Effector :", P[-1][:3, 3])
         print("Error :", err)
     else:
-        print("IK error")
+        print("\nIK error\n")
+        print("Angle :", angle)
         print("Target :", target)
-        print("EEF :", P3[:3, 3])
+        print("End Effector :", P[-1][:3, 3])
         print("Error :", err)
+    plt.show()
+
+def main():
+    fig.canvas.mpl_connect('button_press_event', onclick)
+    fig.suptitle("Cyclic Coordinate Descent - Inverse Kinematics", fontsize=12)
+    ax.set_xlim(-50, 150)
+    ax.set_ylim(-50, 150)
+
+    # Forward Kinematics
+    P = FK(angle, link)
+    # Plot Link
+    for i in range(len(link)):
+        start_point = P[i]
+        end_point = P[i+1]
+        ax.plot([start_point[0,3], end_point[0,3]], [start_point[1,3], end_point[1,3]], linewidth=5)
+        # draw_axis(ax, scale=5, A=P[i+1], draw_2d=True)
     plt.show()
 
 if __name__ == "__main__":
